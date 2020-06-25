41a8c20965f32cddb50f3d75844b8f27dba4226b
AlanEstudo <alanjuniorestudo@gmail.com>
2020-06-25 07:06:00 -0300

    iniciando metodo de aumento 10%

--- src/appEmpregado.java
+++ src/appEmpregado.java
@@ -11,16 +11,26 @@
 public class appEmpregado {
 
     public static void main(String[] args) {
-        Empregado empregado1 = new Empregado("Alan", "Junior", -1500);
-        
-       String nome = empregado1.getNome();
-       String sobreNome = empregado1.getSobreNome();
-       float salario = empregado1.salario();
-       
+        Empregado empregado1 = new Empregado("Alan", "Junior", 1800);
+        Empregado empregado2 = new Empregado("Camila", "Toledo", 3000);
+
+        String nome1 = empregado1.getNome();
+        String sobreNome1 = empregado1.getSobreNome();
+        float salario1 = empregado1.salario();
+
+        String nome = empregado2.getNome();
+        String sobreNome = empregado2.getSobreNome();
+        float salario = empregado2.salario();
+
+        System.out.println(" Nome :" + nome1);
+        System.out.println(" Sobre nome  :" + sobreNome1);
+        System.out.println(" Salario Mensal:" + salario1);
+        System.out.println(" aumento de 10% :");
+        System.out.println(" --------------------------------------");
         System.out.println(" Nome :" + nome);
-        System.out.println(" Sobre nome  :" + nome);
-        System.out.println(" Salario :" + salario);
-       
+        System.out.println(" Sobre nome  :" + sobreNome);
+        System.out.println(" Salario Mensal :" + salario);
+        System.out.println(" aumento de 10% :");
     }
 
 }
