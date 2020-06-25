5fd59de57ac577de3c699f7e9a4d0563e452e8bb
AlanEstudo <alanjuniorestudo@gmail.com>
2020-06-25 06:43:26 -0300

    Inicando a Classe appEmpregado

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,13 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Alan <alanjuniorestudo@gmail.com>
  */
 public class Empregado {
+
+    private String nome;
+    private String sobreNome;
+    private float salarioMensal;
+
+    public Empregado(String nome, String sobreNome, float salarioMensal) {
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+        this.salarioMensal = salarioMensal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public float getSalarioMensal() {
+        if (salarioMensal < 0) {
+            salarioMensal = 0.00f;
+        }
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(float salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
 
 }
